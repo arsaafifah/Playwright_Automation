5bbf1307854e32d697d3fe1399f9a85f8744a8ce
arsaafifah <arsa.afifah@gmail.com>
2026-07-21 13:00:21 +0700

    'additional_negative_test_case'

--- tests/xeltiusTest.spec.ts
+++ tests/xeltiusTest.spec.ts
@@ -3,23 +3,36 @@ import {LoginXeltius} from'../pages/xeltiusPage'
 
 let loginXeltius : LoginXeltius ;
 
+test.describe ('login', function () {
+
 test.beforeEach(async({page}) => {
 
     loginXeltius = new LoginXeltius (page);
 
     await loginXeltius.openWebsite();
 
-})
+});
 
 
-test('success login', async({page})=>{
+test('success login', async()=>{
 
     const email = ('arsa.afifah@gmail.com');
     const password =('Afifah_071002');
 
     await loginXeltius.loginUser(email, password);
 
-}) 
+}); 
+
+test('failed login', async({page})=>{
+
+    const email = ('arsa.afifah@gmail.com');
+    const password =('Afifah_07');
+
+    await loginXeltius.loginUser(email, password);
+
+    await expect(page.locator("//div[@class='css-1u40kae']")).toBeVisible();
+
+}); 
 
 
-
+});
